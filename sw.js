b79a4b725febf39a994ee6bd9a1bc6414e41535b
Wedja Sousa <wedja-maria2010@hotmail.com>
2025-10-05 10:09:47 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "photo-journal-v1"
+const urlsToCache = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json"]
+
+// Instalação do Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
+})
+
+// Ativação do Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        }),
+      )
+    }),
+  )
+})
+
+// Interceptar requisições
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    }),
+  )
+})
